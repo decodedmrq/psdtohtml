5ad75559280dfc53c8c0ea1a40c1a8b01cbefafe
Nguyen Van Quyen <quyennv.bg@gmail.com>
2017-04-12 21:59:11 +0700

    config

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -8,16 +8,9 @@ window._ = require('lodash');
  */
 
 window.$ = window.jQuery = require('jquery');
+window.Tether = require('tether');
+require('bootstrap');
 
-require('bootstrap-sass');
-
-/**
- * Vue is a modern JavaScript library for building interactive web interfaces
- * using reactive data binding and reusable components. Vue's API is clean
- * and simple, leaving you to focus on building your next great project.
- */
-
-window.Vue = require('vue');
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
