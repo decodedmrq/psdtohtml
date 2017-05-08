95b0ea026447dad3118dfae3f5f5ac0ee4818cc0
nuocgansoi <nuocgansoi@gmail.com>
2017-05-08 06:15:21 +0000

    [Layout] home page: change feature images

--- resources/views/home/feature/item.blade.php
+++ resources/views/home/feature/item.blade.php
@@ -4,7 +4,7 @@
         <div class="bg-pink-light" id="bg-feature"></div>
     @endif
     <div class="row">
-        <div class="col-sm-6 {{ !is_odd_number($index) ? 'push-sm-6' : '' }}">
+        <div class="col-sm-6 {{ !is_odd_number($index) ? 'push-sm-6 content-right' : '' }}">
             <div class="h-100 d-flex align-items-center">
                 <div>
                     <div class="title">{{ $feature['name'] }}</div>
@@ -13,14 +13,13 @@
                             <li>{{$content}}</li>
                         @endforeach
                     </ul>
-                    {{--<p class="content">{{ $feature->content }}</p>--}}
                 </div>
             </div>
         </div>
 
         <div class="col-sm-6 {{ !is_odd_number($index) ? 'pull-sm-6' : '' }}">
             <div class="row {{ is_odd_number($index) ? 'justify-content-end' : '' }}">
-                <div class="col-sm-7">
+                <div class="col-sm-12">
                     <img src="{{ $feature['image'] }}" alt="{{ $feature['name'] }}">
                 </div>
             </div>
