6b72b6f0c68d18f4baae86f47a056b3a41624a8b
nuocgansoi <nuocgansoi@gmail.com>
2017-06-02 11:13:55 +0000

    [Common] add errors trans

---
+++ resources/lang/vi/errors.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: nuocgansoi
+ * Date: 6/2/2017
+ * Time: 5:58 PM
+ */
+
+return [
+    '404' => 'Không tìm thấy trang yêu cầu',
+    'click_to_back' => 'Nhấn vào đây để quay trở lại.'
+];
